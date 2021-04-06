0afcd0d1d9e66d5860e5c3aa2d37929fd26f792e
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-04-06 22:59:29 +0900

    Initial Commit

---
+++ HelpNagoyaSpecialty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelpNagoyaSpecialty
+//
+//  Created by YashimaMasafumi on 2021/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
