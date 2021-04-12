41b3cfedb7baa1f96697a033087ab3ab256bb0ae
Masa.Yashi <myaassahfiummai.10.11@gmail.com>
2021-04-13 00:19:40 +0900

    GameSceneの設定完了 画像取り込み完了

---
+++ HelpNagoyaSpecialty/GameScene.swift
@@ -0,0 +1,12 @@
+//
+//  GameScene.swift
+//  HelpNagoyaSpecialty
+//
+//  Created by YashimaMasafumi on 2021/04/12.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+
+}
--- HelpNagoyaSpecialty/ViewController.swift
+++ HelpNagoyaSpecialty/ViewController.swift
@@ -6,11 +6,29 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //SKViewに型を変換
+        //StoryBoardと関連づける
+        let skView = self.view as! SKView
+        
+        //FPSの表示
+        skView.showsFPS = true
+        
+        //ノード数の表示
+        skView.showsNodeCount = true
+        
+        //シーンの作成
+        let scene = GameScene(size: skView.frame.size)
+        
+        //ビュー上にシーンを表示する
+        skView.presentScene(scene)
+        
         // Do any additional setup after loading the view.
     }
 
